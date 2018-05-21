680d1294bbdd8731592f93915dbd29e116e64632
Alen Begović <abegovic@int.sedamIT.hr>
2018-05-21 14:31:13 +0200

    first POC

--- src/FormGen.js
+++ src/FormGen.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
- 
+
 import Form from "react-jsonschema-form";
- 
+
+
 const formData = {
-    title: "First task",
-    done: true
-  };
+  title: "First task",
+  done: true
+};
 
 const schema = {
   title: "Todo",
   type: "object",
   required: ["title"],
   properties: {
-    title: {type: "string", title: "Title", default: "A new task"},
-    done: {type: "boolean", title: "Done?", default: false}
+    title: { type: "string", title: "Title", default: "A new task" },
+    done: { type: "boolean", title: "Done?", default: false }
   }
 };
- 
+
 const log = (type) => console.log.bind(console, type);
- 
-render((
-  <Form schema={schema}
+
+function FormGen() {
+
+
+    return (
+      <Form schema={schema}
         formData={formData}
         onChange={log("changed")}
         onSubmit={log("submitted")}
         onError={log("errors")} />
-), document.getElementById("app"));
+    )
+
+}
+
+export default FormGen;
+

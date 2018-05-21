d1baa4a4613db41132d6dc3c8a58ccb89f33dc11
Alen Begović <abegovic@int.sedamIT.hr>
2018-05-21 14:32:04 +0200

    Adding Simple Form Gen, Validate & Utils

---
+++ src/playground/index.js
@@ -0,0 +1,12 @@
+import simple from "./simple";
+
+
+export const samples = {
+  Simple: simple,
+  "Date & time": date,
+  "Custom Array": customArray,
+  "Custom Object": customObject,
+  Alternatives: alternatives,
+  "Property dependencies": propertyDependencies,
+  "Schema dependencies": schemaDependencies,
+};

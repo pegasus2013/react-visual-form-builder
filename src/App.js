83bbfe2ce2763805b99fd07dd7a277cf3a6d805f
Alen Begović <abegovic@int.sedamIT.hr>
2018-05-21 14:30:47 +0200

    Adding PlayGround component in App

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FormGen from './FormGen';
+
+import { PlayGround } from './playground/PlayGround';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Sample BB8 FormGen Form</h1>
+
+        <PlayGround />
+        { /*<FormGen /> */}
+
+
       </div>
     );
   }

a03342ceab62c0ee0f287fb424b9f7757c1e26c8
Alen Begović <abegovic@int.sedamIT.hr>
2018-05-28 14:17:30 +0200

    import newelly transformed scss/css in index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //import './index.css';
+import './react-form-builder/css/application.css';
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
